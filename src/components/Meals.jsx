456f723dff2e6397eb34f48df72c9d758877aa02
edakaraman <95571155+edakaraman@users.noreply.github.com>
2023-02-08 22:53:55 +0300

    Some information of the food data taken with axios is displayed on the site.

---
+++ src/components/Meals.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { useGlobalContext } from "../context"
+import {FcLike} from "react-icons/fc"
+
+const Meals = () => {
+    const {meals,loading,selectMeal,addToFavorites} = useGlobalContext()
+
+   if(loading){
+    return(
+      <section className="section">
+        <h5> Loading ...</h5>
+      </section>
+    )
+   }
+   if(meals.length <1){
+    return(
+      <section className="section">
+       <h2> No meals matched your searched term.Please try again.</h2>
+      </section>
+    )
+   }
+
+    return <section className="section_center">
+             {meals.map((singleMeal) => {
+               const {idMeal,strMeal:title,strMealThumb:image} = singleMeal
+               return <article key={idMeal} className="single_meal">
+                <img src={image} className="img" onClick={() => selectMeal(idMeal)} />
+                <footer>
+                  <h5> {(title).substring(0,16)} </h5>
+                  <button onClick={() => addToFavorites(idMeal)}  className="like_btn"> <FcLike/> </button>
+                </footer>
+               </article>
+             })}
+        </section>
+    
+}
+export default Meals
