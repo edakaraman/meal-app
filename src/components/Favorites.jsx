a4552045e151c4e5cba2a3662579c9bbff3b1ddc
edakaraman <95571155+edakaraman@users.noreply.github.com>
2023-02-08 22:50:12 +0300

    Favorites component added.

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,24 @@
+import { useGlobalContext } from "../context"
+import {FcDislike} from "react-icons/fc"
+const Favorites = () => {
+    const {favorites,selectMeal,removeFromFavorites} = useGlobalContext()
+    return (
+        <section className="favorites">
+          <div className="favorites_content">
+            <h4> <b> Favorites  </b> </h4>
+            <div className="favorites_container">
+                {favorites.map((item) => {
+                  const {idMeal ,strMealThumb:image} = item
+                  return (
+                    <div className="favorites_item" key={idMeal}>
+                      <img src={image} onClick={() => selectMeal(idMeal,true)} className="favorites_img" /> <br/>
+                      <button className="btn_remove" onClick={() => removeFromFavorites(idMeal)}> <FcDislike size={28}/> </button>                 
+                    </div>
+                  )
+                })}
+            </div>
+          </div>
+        </section> 
+    )
+}
+export default Favorites
